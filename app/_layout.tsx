269bf8cfe2a38ecd26e34311338ea734fbf6fc17
Mimo <mimoriam420@gmail.com>
2024-12-12 01:36:03 +0500

    build(app): custom dev client & clerk init
    * Enabled development build for android
    * clerk initialization

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
 import { Stack } from 'expo-router';
 
+import { tokenCache } from '~/utils/cache';
+
+const InitialLayout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
+
 export default function Layout() {
-  return <Stack />;
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env');
+  }
+
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,11 @@
-import { Stack } from 'expo-router';
 import React from 'react';
 import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
+    <SafeAreaView>
       <Text>Hello</Text>
-    </>
+    </SafeAreaView>
   );
 }
