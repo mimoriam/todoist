aa373f09fd6975ed487f44006288142932e6b415
Mimo <mimoriam420@gmail.com>
2024-12-11 18:27:23 +0500

    build(app): eslint & imports sort
    * Added Eslint & Prettier configs
    * Added sorting imports via Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['universe/native', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
